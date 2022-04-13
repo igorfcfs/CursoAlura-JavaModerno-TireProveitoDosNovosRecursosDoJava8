10ac8f525da63f1c7b0baa31a3b83117920ab4ce
igorfcfs <igorfcfs@gmail.com>
2022-04-13 14:44:58 -0300

    Mais Streams, Collectors e APIs

--- java8/src/ExemploCursos.java
+++ java8/src/ExemploCursos.java
@@ -1,6 +1,10 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
 
 class Curso {
 
@@ -19,11 +23,6 @@ class Curso {
 	public int getAlunos() {
 		return alunos;
 	}
-
-//	@Override
-//	public String toString() {
-//		return "Nome: " + nome + " - " + "Alunos: " + alunos;
-//	}
 }
 
 public class ExemploCursos {
@@ -33,27 +32,52 @@ public class ExemploCursos {
 		cursos.add(new Curso("JavaScript", 150));
 		cursos.add(new Curso("Java 8", 113));
 		cursos.add(new Curso("C", 55));
-		
+
 		cursos.sort(Comparator.comparingInt(Curso::getAlunos));
-		
-//		cursos.forEach(c -> System.out.println(c.getNome()));
 
+		OptionalDouble media = cursos.stream().filter(c -> c.getAlunos() >= 100).mapToInt(Curso::getAlunos).average();
+
+//		System.out.println(media);
+		
+//		Optional<Curso> optionalCurso = cursos.stream().filter(c -> c.getAlunos() >= 100).findAny();
+		
+////		Curso curso = optionalCurso.orElse(null);
+////		System.out.println(curso.getNome());
+		
+//		optionalCurso.ifPresent(c -> System.out.println(c.getNome()));
+		
 //		cursos.stream()
 //			.filter(c -> c.getAlunos() >= 100)
-//			.forEach(c -> System.out.println(c.getNome()));
-
+//			.findAny()
+//			.ifPresent(c -> System.out.println(c.getNome()));
+		
+//		List<Curso> resultado = cursos.stream()
+//			.filter(c -> c.getAlunos() >= 100)
+//			.collect(Collectors.toList());
+		
+//		cursos = cursos.stream()
+//				.filter(c -> c.getAlunos() >= 100)
+//				.collect(Collectors.toList());
+//		
+//		cursos.stream().forEach(c -> System.out.println(c.getNome()));
+		
+//		Map<String, Integer> map = cursos.stream()
+//				.filter(c -> c.getAlunos() >= 100)
+//				.collect(Collectors.toMap(
+//						c -> c.getNome(),
+//						c -> c.getAlunos()));
+//		
+//		System.out.println(map);
+		
 //		cursos.stream()
 //			.filter(c -> c.getAlunos() >= 100)
-//			.map(c -> c.getAlunos())
-//			.forEach(total -> System.out.println(total));
+//			.collect(Collectors.toMap(c -> c.getNome(),c -> c.getAlunos()))
+//			.forEach((nome, alunos) -> System.out.println(nome + " tem " + alunos + " alunos"));
 		
-		cursos.stream()
-			.filter(c -> c.getAlunos() >= 100)
-			.map(Curso::getAlunos)
-			.forEach(System.out::println);
-		
-		int sum = cursos.stream().filter(c -> c.getAlunos() >= 100).mapToInt(Curso::getAlunos).sum();
-		
-		System.out.println(sum);
+		cursos.parallelStream()
+		.filter(c -> c.getAlunos() >= 100)
+		.collect(Collectors.toMap(c -> c.getNome(),c -> c.getAlunos()))
+		.forEach((nome, alunos) -> System.out.println(nome + " tem " + alunos + " alunos"));
 	}
 }
+
